3f9ff70408c8b531c1cfb44831ac9dc7dfcb0eaa
Brayan Aquino <brayanaquinobaav@gmail.com>
2024-11-23 19:11:45 -0500

    Creacion de vista de buscador de usuarios

---
+++ resources/views/livewire/search-users.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <div class="mb-4">
+        <label for="start_date" class="block text-gray-700 dark:text-gray-200">Buscar empleado</label>
+        <select name="tipo_busqueda" id="tipo_busqueda">
+            <option value="1">DNI</option>
+            <option value="2">Nombre</option>
+        </select>
+        <input type="text" name="texto_busqueda" id="texto_busqueda" class="w-full mt-2 p-2 border rounded-r-lg dark:bg-gray-700 dark:text-gray-200">
+    </div>
+    <div class="mb-4">
+        <label for="start_date" class="block text-gray-700 dark:text-gray-200">Nombre:</label>
+        <input type="text" name="start_date" id="start_date" class="w-full mt-2 p-2 border rounded-r-lg dark:bg-gray-700 dark:text-gray-200" readonly>
+    </div>
+</div>
